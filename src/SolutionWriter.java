a9077c8ca856aec6e22a2af3dcce80dff2f0d599
Konstantin Kosev <kkosev97@gmail.com>
2020-02-20 19:33:44 +0000

    Ajde da gi ubiem
    vache syzdavame obekti

---
+++ src/SolutionWriter.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.List;
+
+public class SolutionWriter {
+
+    private String filename;
+
+    public SolutionWriter(String filename){
+        this.filename = filename;
+    }
+
+
+    public void writeToFile(List<Pair<Library,List<Book>>> solution){
+        File file = new File(filename);
+        try {
+            FileWriter out = new FileWriter(file);
+
+            PrintWriter printWriter =new PrintWriter(out);
+            printWriter.println(solution.size());
+            for(Pair<Library,List<Book>> l : solution){
+                printWriter.print(l.getFst().getId());
+                printWriter.print(" ");
+                printWriter.print(l.getSnd().size());
+                printWriter.println();
+
+                for(Book b : l.getSnd()){
+                    printWriter.print(b.getId());
+                    printWriter.print(" ");
+                }
+            }
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
