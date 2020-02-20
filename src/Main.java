7424bf621872e150d169684907a04d1d56053d84
Konstantin Kosev <kkosev97@gmail.com>
2020-02-20 18:33:22 +0000

    Ajde da gi ubiem

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,28 @@ public class Main {
         try {
             Scanner sc =new Scanner(file);
             String line  = sc.nextLine();
+           String[] strings =   line.split(" ");
+           int numOfBooks = Integer.parseInt(strings[0]);
+           int numOfLibraries = Integer.parseInt(strings[1]);
+           int days = Integer.parseInt(strings[2]);
+           line = sc.nextLine();
+           strings = line.split(" ");
+           for(int i = 0 ;i < strings.length;i++){
+               System.out.println( "Book " + i + " has score: " + Integer.parseInt(strings[i]));
+           }
+
+           for(int i = 0; i < numOfLibraries;i++){
+               line = sc.nextLine();
+               strings = line.split(" ");
+               System.out.println( "Library " + i + " has num of books " + strings[0] + " signup time " + strings[1] +  " and number of books per day " + strings[2]);
+
+               line = sc.nextLine();
+               strings = line.split(" ");
+               for(String s : strings){
+                   System.out.print( "Book id" + Integer.parseInt(s) + " ");
+               }
+               System.out.println();
+           }
 
 
         } catch (FileNotFoundException e) {
