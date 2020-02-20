9d73a907ade23afaed99ec01ea615126ebda0ab4
Daniel Stanislavov Atanasov CSM2016 <daniel.atanasov.2016@uni.strath.ac.uk>
2020-02-20 19:58:07 +0000

    Calculate Library Score

--- src/CalculateLibraryScoreAvg.java
+++ src/CalculateLibraryScoreAvg.java
@@ -5,6 +5,9 @@ public class CalculateLibraryScoreAvg implements CalculateLibraryScore {
     CalculateBookScoreAvg cbsa;
 
     @Override
+    //Calculates Library score
+    //Inputs : library - The library to calculate
+    //       : books   - A list of all books
     public double calculate(Library library, List<Book> books) {
         double score = 0;
         score = ( cbsa.calculate(library.getBooks() ) * library.getBooksPerDay() ) - ( library.getDays() * cbsa.calculate(books) );
