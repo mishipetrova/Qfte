d7b9d18816608fac888ed0b77193c4a9b5acdada
Konstantin Kosev <kkosev97@gmail.com>
2020-02-20 18:47:30 +0000

    Ajde da gi ubiem
    vache syzdavame obekti

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         File file = new File("a_example.txt");
+        List<Library> libraries = new ArrayList<>();
+        Map<Integer,Integer> scoreMap = new HashMap<Integer,Integer>();
+
         try {
             Scanner sc =new Scanner(file);
             String line  = sc.nextLine();
@@ -16,18 +20,27 @@ public class Main {
            strings = line.split(" ");
            for(int i = 0 ;i < strings.length;i++){
                System.out.println( "Book " + i + " has score: " + Integer.parseInt(strings[i]));
+               scoreMap.put(i,Integer.parseInt(strings[i]));
            }
 
            for(int i = 0; i < numOfLibraries;i++){
                line = sc.nextLine();
                strings = line.split(" ");
+
                System.out.println( "Library " + i + " has num of books " + strings[0] + " signup time " + strings[1] +  " and number of books per day " + strings[2]);
+                int libraryId = i;
+                int numBooks = Integer.parseInt(strings[0]);
+                int signup = Integer.parseInt(strings[1]);
+                int booksPerDay = Integer.parseInt(strings[2]);
+                List<Book> books = new ArrayList<>();
 
                line = sc.nextLine();
                strings = line.split(" ");
                for(String s : strings){
+                   books.add(new Book(Integer.parseInt(s),scoreMap.get(Integer.parseInt(s))));
                    System.out.print( "Book id" + Integer.parseInt(s) + " ");
                }
+               libraries.add(new Library(books,signup,booksPerDay));
                System.out.println();
            }
 
